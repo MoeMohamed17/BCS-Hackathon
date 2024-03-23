e8cfed448a3710366e6821793b3eb8cc507225dc
moemohamed <moemoe17@students.cs.ubc.ca>
2024-03-23 13:16:07 -0700

    updated discord key and created llm model

---
+++ chatgpt.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import constants
+import discord 
+import warnings
+
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain_community.llms import OpenAI
+from langchain_community.vectorstores import Chroma
+from discord.ext import commands 
+
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
+
+# Enable to save to disk & reuse the model (for repeated queries on the same data)
+PERSIST = False
+
+query = None
+if len(sys.argv) > 1:
+  query = sys.argv[1]
+
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+  loader = DirectoryLoader("data/")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+  llm=ChatOpenAI(model="gpt-3.5-turbo"),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
